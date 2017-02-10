f38c569953923fdf6e8b39d5b990fa76b55b2a04
Bastiaan Roos <bastiaan@roosgeo.nl>
2017-02-10 13:41:28 +0100

    function get midpoints

--- test/test_tool_connect_start_end_points.py
+++ test/test_tool_connect_start_end_points.py
@@ -4,13 +4,13 @@ import os.path
 import fiona
 
 from utils.collection import MemCollection
-from tools.connect_start_end_points import get_start_endpoints
+from tools.connect_start_end_points import get_start_endpoints, get_midpoints
 
 
 test_data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
 
 
-class TestTLine(unittest.TestCase):
+class TestTools(unittest.TestCase):
 
     def setUp(self):
         pass
@@ -39,5 +39,26 @@ class TestTLine(unittest.TestCase):
         point_col = get_start_endpoints(collection, ['id', 'name'])
 
         self.assertEqual(len(point_col), 4)
+        self.assertDictEqual(point_col[0]['properties'],
+                             {'id': 1L, 'name': 'test name 1'})
+
+    def test_get_midpoints_mem_collection(self):
+        collection = MemCollection(geometry_type='Linestring')
+
+        collection.writerecords([
+            {'geometry': {'type': 'LineString',
+                          'coordinates': [(0.0, 0.0), (4.0, 4.0)]},
+             'properties': {'id': 1L, 'name': 'test name 1'}},
+            {'geometry': {'type': 'LineString',
+                          'coordinates': [(2.0, 0.0), (0.0, 2.0)]},
+             'properties': {'id': 2L, 'name': 'line crosses line 1'}}
+        ])
+
+        point_col = get_midpoints(collection, ['id', 'name'])
+
+        self.assertEqual(len(point_col), 2)
+        self.assertDictEqual(point_col[0]['geometry'],
+                             {'type': 'Point',
+                              'coordinates': (2.0, 2.0)})
         self.assertDictEqual(point_col[0]['properties'],
                              {'id': 1L, 'name': 'test name 1'})
--- tools/connect_start_end_points.py
+++ tools/connect_start_end_points.py
@@ -1,5 +1,6 @@
 from shapely.geometry import shape
 from utils.collection import MemCollection
+from utils.geometry import TLine, TMultiLineString
 
 
 def get_start_endpoints(line_col, copy_fields=[]):
@@ -22,5 +23,26 @@ def get_start_endpoints(line_col, copy_fields=[]):
     return point_col
 
 
+def get_midpoints(line_col, copy_fields=[]):
+    """ returns MemCollection with start and end points of line"""
 
+    point_col = MemCollection(geometry_type='Point')
 
+    for feature in line_col.filter():
+        try:
+            line = TLine(feature['geometry']['coordinates'])
+        except ValueError:
+            line = TMultiLineString(feature['geometry'])
+
+        props = {}
+        for field in copy_fields:
+            props[field] = feature['properties'].get(field, None)
+
+        coord = line.get_point_at_percentage(0.5).coords[0]
+
+        point_col.writerecords([
+            {'geometry': {'type': 'Point',
+                          'coordinates': line.get_point_at_percentage(0.5)},
+             'properties': props}])
+
+    return point_col
