1408de37cd867f498ed34a1f8f8f23aa2d59a594
AnnemiekeVogel <a.vogel@tijhuisingenieurs.nl>
2017-02-10 09:36:01 +0100

    New methods for TLine + extra WIT functions
    Onderscheid gemaakt in TLine methods op basis van een afstand en op
    basis van een point. Bij Point wordt eerste een conversie gedaan naar
    afstand en vervolgens de method op basis van afstand aangeroepen.
    
    De WIT set wordt een set bewerkingen die gebruik maken van de
    geometry.py classes.

--- utils/geometry.py
+++ utils/geometry.py
@@ -3,6 +3,7 @@ from math import sqrt
 from sympy.physics.units import length
 
 import logging
+from sympy.geometry.line import Segment
 logger = logging.getLogger(__name__)
 
 
@@ -11,8 +12,37 @@ class TLine(LineString):
     def __init__(self, *args, **kwargs):
         self._length_array = None
         super(TLine, self).__init__(*args, **kwargs)
+        
 
-    def get_line_part(self, point):
+    def get_line_part_dist(self, afstand):
+        """ get vertex before and after distance on line
+
+        afstand: distance to find on line
+        return: tuple with:
+                vertex before as tuple with: (nr of vertex on line, vertex 
+                    coordinates, distance on line)
+                vertex after as tuple with: (nr of vertex on line, vertex 
+                    coordinates, distance on line)
+                distance used to find segment
+        """
+        # todo: make sure function is correct when point is on start vertex
+
+        if self._length_array is None:
+            self._length_array = [(i, p, self.project(Point(p))) 
+                                  for i, p in enumerate(self.coords)]
+
+        dist = afstand
+
+        for p in self._length_array:
+            if dist <= p[2]:
+                vertex_before = self._length_array[p[0] - 1]
+                vertex_after = p
+                break
+        line_part = (vertex_before, vertex_after, dist)
+        
+        return line_part
+
+    def get_line_part_point(self,point):
         """ get vertex before and after point on line
 
         point (shapely.Point): point to find on or near line
@@ -21,33 +51,39 @@ class TLine(LineString):
                     coordinates, distance on line)
                 vertex after as tuple with: (nr of vertex on line, vertex 
                     coordinates, distance on line)
-                distance of point on line
+                distance used to find segment
         """
-        # todo: make sure function is correct when point is on start vertex
+        afstand = self.project(point)
+        line_part = self.get_line_part_dist(afstand)
+    
+        return line_part
+    
+    def get_line_part_perc(self,perc):
+        """ get vertex before and after percentage of line
 
-        if self._length_array is None:
-            self._length_array = [(i, p, self.project(Point(p))) 
-                                  for i, p in enumerate(self.coords)]
-
-        dist = self.project(point)
-
-        for p in self._length_array:
-            if dist <= p[2]:
-                vertex_before = self._length_array[p[0] - 1]
-                vertex_after = p
-                break
-
-        return (vertex_before, vertex_after, dist)
-
-    def get_segment_richting(self, point):
-        """ get vertex before and after point on line
+        perc: percentage of total line length.
+                    decimal values between 0 and 1
+        return: tuple with:
+                vertex before as tuple with: (nr of vertex on line, vertex 
+                    coordinates, distance on line)
+                vertex after as tuple with: (nr of vertex on line, vertex 
+                    coordinates, distance on line)
+                distance used to find segment
+        """
+        afstand = self.length * perc
+        line_part = self.get_line_part_dist(afstand)
+    
+        return line_part
+    
+    def get_segment_richting_dist(self, afstand):
+        """ get vertex before and after distance on line
         and get direction of line segment
 
         point (shapely.point): point to find on or near line
         return: direction of segment as tuple [dx,dy]
         """
         
-        segment = self.get_line_part(point)
+        segment = self.get_line_part_dist(afstand)
         
         vertex_before = segment[0][1]
         vertex_after = segment[1][1]
@@ -56,19 +92,33 @@ class TLine(LineString):
         delta_y = vertex_after[1]-vertex_before[1]
         richting = [delta_x,delta_y]
         
-        return (richting)
+        return richting
         
+    def get_segment_richting_point(self, point):
+        """ get vertex before and after distance on line
+        and get direction of line segment
+
+        point (shapely.point): point to find on or near line
+        return: direction of segment as tuple [dx,dy]
+        """
+        
+        afstand = self.project(point)
+        richting = self.get_segment_richting_dist(afstand)
     
-    def get_haakselijn(self, point, length):
-        """ create line trough point on line perpendicular to direction of 
-        coordinate set uses get_lin_part
+        return richting
+            
+    
+    def get_haakselijn_point(self, point, length):
+        """ create line at distance on line perpendicular to direction of 
+        coordinate set
+        uses get_line_part
 
         point (shapely.point): point at which to create perpendicular
         length: length of perpendicular to create
         return: Tline with 3 vertices (point left, point on line, point right)
         """
 
-        richting = self.get_segment_richting(point)
+        richting = self.get_segment_richting_point(point)
         
 #         richting segment = (delta x,delta y)
 #         haakse richting = -delta x / delta y
@@ -116,6 +166,54 @@ class TLine(LineString):
         
         return haakselijn
 
+
+    
+    def get_point_at_distance(self,afstand):
+        """ create a point on a line at a given distance from the line origin 
+
+        afstand: distance from line origin to create point at
+        return: point (shapely.point)    
+        """
+        segment = self.get_line_part_dist(afstand)
+        vertex_before = segment[0][1]
+        vertex_after = segment[1][1]
+        
+        delta_x_segment = vertex_after[0]-vertex_before[0]
+        delta_y_segment = vertex_after[1]-vertex_before[1]
+        
+        richting_segement = [delta_x_segment,delta_y_segment]
+        lengte_segment = segment[1][2] - segment[0][2]
+        
+#         bereken verschil in coordinaten voor elke meter langs segment:
+        delta_x_m = delta_x_segment / lengte_segment
+        delta_y_m = delta_y_segment / lengte_segment
+
+#         bereken restlengte in Segment
+        afstand_segment = afstand - segment[0][2]   
+       
+          
+#         bepaal coordinaten van het nieuwe punt
+        point_x = vertex_before[0] + (afstand_segment * delta_x_m)
+        point_y = vertex_before[1] + (afstand_segment * delta_y_m)
+         
+#         point = (delta_x_m,delta_y_m)
+        point = Point(point_x,point_y)
+                
+        return point
+
+    def get_point_at_percentage(self,perc):
+        """ create a point on a line at a given percentage of total line length
+        from the line origin 
+
+        perc: percentage from line length to create point at
+        return: point (shapely.point)    
+        """
+        
+        afstand = self.length * perc
+        point = self.get_point_at_distance(afstand) 
+        
+        return point
+        
 
 class TMultiLineString(object):
 
