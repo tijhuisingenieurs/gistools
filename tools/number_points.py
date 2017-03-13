c3f321f93dd9c272615f03b838287fe22b8a3574
Bastiaan Roos <bastiaan@roosgeo.nl>
2017-03-13 14:55:15 +0100

    renumber tool ignores 0 numbers on lines and input for first point number
    fixes /tijhuisingenieurs/TijhuisArcGisTools#28

--- tools/number_points.py
+++ tools/number_points.py
@@ -6,12 +6,25 @@ def number_points_on_line(lines,
                           points,
                           line_number_field='nr',
                           line_direction_field=None,
-                          point_number_field='nr'):
+                          point_number_field='nr',
+                          start_number=1):
+    """ Renumber the points on a set of numbered lines
 
-    sorted_lines = [l for l in lines]
+    lines (collection of LineString of MultiLineString): lines with a number
+    points (collection of Point): points which will be renumbered
+    line_number_field (string): property name of line with order number. The order is ascending and the 0 will be
+            ignored. preferably use a integer property/ field
+    line_direction_field (string): optional - property/ field indicate of the points must be numbered in
+            the direction of the shape (for values >= 0) or in opposite direction (for values < 0)
+    point_number_field (string): output property/ field of the new (renumbered) index
+    start_number (int): first number in index range
+    :return:
+    """
+
+    sorted_lines = [l for l in lines if l['properties'][line_number_field] not in (0, 0.0, '', None)]
     sorted_lines.sort(key=lambda li: li['properties'][line_number_field])
 
-    i = 0
+    i = start_number
 
     for point in points:
         point['properties'][point_number_field] = None
