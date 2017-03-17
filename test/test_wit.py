5b69da5baa533a1021edf8ae462094e5835f29f5
Bastiaan Roos <bastiaan@roosgeo.nl>
2017-03-17 09:38:23 +0100

    fixed test en pep8

--- test/test_wit.py
+++ test/test_wit.py
@@ -1,7 +1,6 @@
 import unittest
 from gistools.utils.wit import *
 
-# TODO: uitzoeken hoe OrderedDict goed af te dwingen in genereren van dict en in AssertDictEqual
 
 class TestWit(unittest.TestCase):
 
@@ -10,10 +9,8 @@ class TestWit(unittest.TestCase):
 
     def test_vul_leggerwaarden(self):
         """test create values of theoretical profiel"""
-        
         pass
-    
-    
+
     def test_create_leggerpunten(self):
         """test create points of theoretical profiel"""
         
@@ -25,36 +22,34 @@ class TestWit(unittest.TestCase):
         ti_knkbodr = 8
 
         profiel_dict = create_leggerpunten(line, line_id, name, ti_waterbr, ti_talulbr, ti_knkbodr)
-             
-        
-#     def test_update_leggerpunten_diepten(self):
-#         """test update of profiel_dict with depths of theoretical profiel"""
-#         
-#         profiel_dict = {'id' : 1, 'name': 'test lijn',
-#                     'L22': (-5.0, 5.0), 'L22_peil': 0.00,
-#                     'knik_l': (-2.0, 5.0), 'knik_l_dpt': 0.00,
-#                     'knik_r': (3.0, 5.0), 'knik_r_dpt': 0.00,
-#                     'R22': (5.0, 5.0), 'R22_peil': 0.00}
-#         
-#         ti_velden_dict = {'ti_waterp': -1.0, 
-#                  'ti_diepte' : 2.0,
-#                  'ti_waterbr' : 9.0,
-#                  'ti_bodemh' : -3.0,
-#                  'ti_bodembr' : 5.0,
-#                  'ti_talulbr' : 2.0,
-#                  'ti_talurbr': 2.0,
-#                  'ti_knkbodr': 7.0}  
-#         
-#         profiel_dict = update_leggerpunten_diepten(profiel_dict, ti_velden_dict)
-# 
-#         self.assertDictEqual(profiel_dict,
-#                              {'id' : 1, 'name': 'test lijn',
-#                               'L22': (-5.0, 5.0), ':L22_peil': -1.00,
-#                               'knik_l': (-2.0, 5.0), 'knik_l_dpt': -3.0,
-#                               'knik_r': (3.0, 5.0), 'knik_r_dpt': -3.0,
-#                               'R22': (5.0, 5.0), 'R22_peil': -1.0,
-#                               'ti_talulbr': 2.0,
-#                               'ti_knkbodr': 7.0,
-#                               'ti_waterbr': 9.0})
-#         
-# These assertions do not work because they are not ordered... need to check why dicts are not ordered..       
+
+    def test_update_leggerpunten_diepten(self):
+        """test update of profiel_dict with depths of theoretical profiel"""
+
+        profiel_dict = {'id': 1, 'name': 'test lijn',
+                        'L22': (-5.0, 5.0), 'L22_peil': 0.00,
+                        'knik_l': (-2.0, 5.0), 'knik_l_dpt': 0.00,
+                        'knik_r': (3.0, 5.0), 'knik_r_dpt': 0.00,
+                        'R22': (5.0, 5.0), 'R22_peil': 0.00}
+
+        ti_velden_dict = {'ti_waterp': -1.0,
+                          'ti_diepte': 2.0,
+                          'ti_waterbr': 9.0,
+                          'ti_bodemh': -3.0,
+                          'ti_bodembr': 5.0,
+                          'ti_talulbr': 2.0,
+                          'ti_talurbr': 2.0,
+                          'ti_knkbodr': 7.0}
+
+        profiel_dict = update_leggerpunten_diepten(profiel_dict, ti_velden_dict)
+
+        self.assertDictEqual(profiel_dict,
+                             {'id': 1, 'name': 'test lijn',
+                              'L22': (-5.0, 5.0), 'L22_peil': -1.00,
+                              'knik_l': (-2.0, 5.0), 'knik_l_dpt': -3.0,
+                              'knik_r': (3.0, 5.0), 'knik_r_dpt': -3.0,
+                              'R22': (5.0, 5.0), 'R22_peil': -1.0})
+        # not in output:
+        # 'ti_talulbr': 2.0,
+        # 'ti_knkbodr': 7.0,
+        # 'ti_waterbr': 9.0
