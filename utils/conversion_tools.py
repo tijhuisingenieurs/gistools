84923ed53f7ed29444d8a8412bfe9b7ebb15d0bf
Bastiaan Roos <bastiaan@roosgeo.nl>
2017-06-21 12:48:06 +0200

    conversion_tools

---
+++ utils/conversion_tools.py
@@ -0,0 +1,22 @@
+
+
+def get_float(value, default=-99.0):
+
+    if value is None:
+        return default
+    elif type(value) == float:
+        return value
+    elif type(value) == int:
+        return float(value)
+    elif type(value) == str:
+        if value == '':
+            return default
+        else:
+            try:
+                return float(value)
+            except ValueError:
+                return default
+    else:
+        print('unknown type! type: {type}, value: {value}.'.format(
+            **{'type': type(value), 'value': value}))
+        return default
