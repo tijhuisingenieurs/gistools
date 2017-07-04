a5c5d86453a80d16ba98fe97fb0d375dda07892f
AnnemiekeVogel <a.vogel@tijhuisingenieurs.nl>
2017-07-04 16:27:56 +0200

    csv_handler with import csv to memcollection + extra tests +rename attributes
    - make attributes consistent over scripts
    - import_csv_to_memcollection
    - testdata for import_csv_to_memcollection

--- tools/create_veldwerk_output_shapes.py
+++ tools/create_veldwerk_output_shapes.py
@@ -28,7 +28,8 @@ def create_fieldwork_output_shapes(line_col, point_col):
     for line in line_col:
 
         line_props = line['properties']
-
+        test = get_float(line_props['breedte'])
+        
         l = {
             'pk': line_props['pk'],
             'ids': line_props['ids'],
@@ -39,7 +40,12 @@ def create_fieldwork_output_shapes(line_col, point_col):
             'breedte': round(get_float(line_props['breedte']), 2)
         }
 
-        line = TLine((line['geometry']['coordinates'][0], line['geometry']['coordinates'][-1]))
+        if type(line['geometry']['coordinates'][0][0]) != tuple:
+            line = TLine([line['geometry']['coordinates'][0], line['geometry']['coordinates'][-1]])
+        else:
+            line = TLine([line['geometry']['coordinates'][0][0],line['geometry']['coordinates'][-1][-1]])
+        
+#         line = TLine((line['geometry']['coordinates'][0], line['geometry']['coordinates'][-1]))
         line = line.get_line_with_length(l['breedte'], 0.5)
 
         for point in point_col:
@@ -55,46 +61,46 @@ def create_fieldwork_output_shapes(line_col, point_col):
                     'datum': l['datum'],
                     'code': point_props['code'],                
                     'afstand': round(get_float(point_props['afstand']),2),
-                    'x': xy[0],
-                    'y': xy[1]                    
+                    'x_coord': xy[0],
+                    'y_coord': xy[1]                    
                 }                             
                 
                 # indien alle hoogtes bekend, vul waarden in
-                if point_props['bk_wp'] <> '' and point_props['ok_wp'] <> '':
-                    p['bk_wp'] = round(get_float(point_props['bk_wp']),2)
-                    p['bk_nap'] = round(get_float(l['wpeil']) - (get_float(point_props['bk_wp']) / 100),2)
-                    p['ok_wp'] = round(get_float(point_props['ok_wp']),2)
-                    p['ok_nap'] = round(get_float(l['wpeil']) - (get_float(point_props['ok_wp']) / 100),2)               
+                if point_props['_bk_wp'] <> '' and point_props['_ok_wp'] <> '':
+                    p['_bk_wp'] = round(get_float(point_props['_bk_wp']),2)
+                    p['_bk_nap'] = round(get_float(l['wpeil']) - (get_float(point_props['_bk_wp']) / 100),2)
+                    p['_ok_wp'] = round(get_float(point_props['_ok_wp']),2)
+                    p['_ok_nap'] = round(get_float(l['wpeil']) - (get_float(point_props['_ok_wp']) / 100),2)               
                 
-                # indien ok_wp onbekend, maar kb_wp bekend, neem bk_wp over en vul waarden in      
-                elif point_props['ok_wp'] == '' and point_props['bk_wp'] <> '':
-                    p['bk_wp'] = round(get_float(point_props['bk_wp']),2)
-                    p['bk_nap'] = round(get_float(l['wpeil']) - (get_float(point_props['bk_wp']) / 100),2)
-                    p['ok_wp'] = round(get_float(point_props['bk_wp']), 2)
-                    p['ok_nap'] = round(get_float(l['wpeil']) - (p['ok_wp'] / 100), 2)
+                # indien _ok_wp onbekend, maar _bk_wp bekend, neem _bk_wp over en vul waarden in      
+                elif point_props['_ok_wp'] == '' and point_props['_bk_wp'] <> '':
+                    p['_bk_wp'] = round(get_float(point_props['_bk_wp']),2)
+                    p['_bk_nap'] = round(get_float(l['wpeil']) - (get_float(point_props['_bk_wp']) / 100),2)
+                    p['_ok_wp'] = round(get_float(point_props['_bk_wp']), 2)
+                    p['_ok_nap'] = round(get_float(l['wpeil']) - (p['_ok_wp'] / 100), 2)
                 
-                # indien bk_wp ondbeken, maar ok_wp bekend, vul ok waarden in en probeer bk_wp te vullen vanuit bk_nap     
-                elif point_props['bk_wp'] == '' and  point_props['ok_wp'] <> '':
-                    p['ok_wp'] = round(get_float(point_props['ok_wp']),2)
-                    p['ok_nap'] = round(get_float(l['wpeil']) - (get_float(point_props['ok_wp']) / 100),2)
+                # indien _bk_wp ondbeken, maar _ok_wp bekend, vul ok waarden in en probeer _bk_wp te vullen vanuit _bk_nap     
+                elif point_props['_bk_wp'] == '' and  point_props['_ok_wp'] <> '':
+                    p['_ok_wp'] = round(get_float(point_props['_ok_wp']),2)
+                    p['_ok_nap'] = round(get_float(l['wpeil']) - (get_float(point_props['_ok_wp']) / 100),2)
                     
-                    if 'bk_nap' in point_props and point_props['bk_nap'] <> 'NoData':
-                        p['bk_wp'] = round(get_float(l['wpeil']) - get_float(point_props['bk_nap']), 2)
-                        p['bk_nap'] = round(get_float(l['wpeil']) - (p['bk_wp'] / 100), 2)
+                    if '_bk_nap' in point_props and point_props['_bk_nap'] <> '-999' and point_props['_bk_nap'] <> '':
+                        p['_bk_wp'] = round(get_float(l['wpeil']) - get_float(point_props['_bk_nap']), 2)
+                        p['_bk_nap'] = round(get_float(l['wpeil']) - (p['_bk_wp'] / 100), 2)
                     else:
-                        log.warning('Geen geldige waarde voor bk_wp te bepalen. bk_wp = ' 
-                                    + point_props['bk_wp'] + ' en bk_nap niet aanwezig')
-                        p['bk_wp'] = point_props['bk_wp']
-                        p['bk_nap'] = 'NoData'
+                        log.warning('Geen geldige waarde voor _bk_wp te bepalen. _bk_wp = ' 
+                                    + point_props['_bk_wp'] + ' en bk_nap niet aanwezig')
+                        p['_bk_wp'] = point_props['_bk_wp']
+                        p['_bk_nap'] = '-999'
                         
                 else:
                         log.warning('Geen geldige waarde voor ok_wp te bepalen. ok_wp = ' 
-                                    + point_props['ok_wp'] + ' en bk_wp = ' + 
-                                    point_props['bk_wp'])
-                        p['bk_wp'] = point_props['bk_wp']
-                        p['bk_nap'] = 'NoData'
-                        p['ok_wp'] = point_props['ok_wp']
-                        p['ok_nap'] = 'NoData'                        
+                                    + point_props['_ok_wp'] + ' en _bk_wp = ' + 
+                                    point_props['_bk_wp'])
+                        p['_bk_wp'] = point_props['_bk_wp']
+                        p['_bk_nap'] = '-999'
+                        p['_ok_wp'] = point_props['_ok_wp']
+                        p['_ok_nap'] = '-999'                        
                 
                         
                 output_point_col.writerecords([{
