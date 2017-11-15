3f89397919b3a980831be50a4da29c34c67167b7
elinevanelburg <33683617+elinevanelburg@users.noreply.github.com>
2017-11-15 16:14:48 +0100

    code for snapping points to line and tests
    tijhuisingenieurs/TijhuisArcGisTools#95

---
+++ tools/snap_points_to_line.py
@@ -0,0 +1,48 @@
+from gistools.utils.collection import MemCollection
+from shapely.geometry import Point, LineString
+
+
+def snap_points_to_line(line_col, points_col, tolerance=0.01, keep_unsnapped_points=False):
+    snapped_points_col = MemCollection(geometry_type='Point')
+    snapped_points_list = []
+
+    for feature in points_col.filter():
+        point = feature['geometry']['coordinates']
+        bbox = (
+            point[0] - tolerance,
+            point[1] - tolerance,
+            point[0] + tolerance,
+            point[1] + tolerance
+        )
+
+        lines = line_col.filter(bbox=bbox)
+
+        point_geom = Point(point)
+        distance = tolerance * 2
+        nearest_line = None
+
+        for line in lines:
+            line_geom = LineString(line['geometry']['coordinates'])
+            new_distance = point_geom.distance(line_geom)
+            if new_distance < distance:
+                distance = new_distance
+                nearest_line = line_geom
+
+        if nearest_line is not None and distance <= tolerance:
+            dist_on_line = nearest_line.project(point_geom)
+            snapped_point = nearest_line.interpolate(dist_on_line)
+            snapped_points_list.append(
+                {'geometry': {'type': 'Point',
+                              'coordinates': snapped_point.coords[0]},
+                 'properties': feature['properties']}
+            )
+
+        else:
+            if keep_unsnapped_points:
+                snapped_points_list.append(feature)
+            else:
+                pass
+
+    snapped_points_col.writerecords(snapped_points_list)
+
+    return snapped_points_col
