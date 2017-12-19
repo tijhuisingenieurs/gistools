a82d5a3ab9bfb043b89bf8c4d54c33beddf09852
elinevanelburg <33683617+elinevanelburg@users.noreply.github.com>
2017-12-19 15:28:17 +0100

    Tests aanpassen op nieuwe versie van metfile generator.

--- utils/metfile_generator.py
+++ utils/metfile_generator.py
@@ -42,6 +42,8 @@ def export_points_to_metfile(point_col, project, metfile_name, codering, type_me
         sorted_points = sorted(unsorted_points, key = lambda x: (x['properties']['prof_ids'],
                                                                  x['properties']['afstand']))
 
+
+
         prefix = None
         if type_peiling != None:
             if type_peiling == "Inpeiling":
