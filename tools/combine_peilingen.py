46cfdd8deb4f6137c0b6f0fb9956ebbce9b45298
elinevanelburg <33683617+elinevanelburg@users.noreply.github.com>
2018-05-31 16:20:26 +0200

    Improve and fix uitpeilingentool

--- tools/combine_peilingen.py
+++ tools/combine_peilingen.py
@@ -1,11 +1,13 @@
 import csv
 import logging
 import numpy as np
+import datetime
+import dateutil.parser as parser
 
 from shapely.geometry import MultiLineString, LineString
 from gistools.utils.xml_handler import import_xml_to_memcollection
 from gistools.utils.collection import MemCollection
-from gistools.utils.metfile_generator import export_points_to_metfile
+from gistools.utils.conversion_tools import get_string
 
 log = logging.getLogger(__name__)
 
@@ -19,36 +21,39 @@ def link_table_to_dict(link_table):
     return link_dict
 
 
-def make_lineString_object(line_collection):
-    if type(line_collection['geometry']['coordinates'][0][0]) != tuple:
-        line = LineString(line_collection['geometry']['coordinates'])
-    else:
-        line = MultiLineString(line_collection['geometry']['coordinates'])
-    return line
+# def make_linestring_object(line_collection):
+#     if type(line_collection['geometry']['coordinates'][0][0]) != tuple:
+#         line = LineString(line_collection['geometry']['coordinates'])
+#     else:
+#         line = MultiLineString(line_collection['geometry']['coordinates'])
+#     return line
 
 
-def combine_profiles(out_points, in_points, scale_factor, scale_bank_distance=False, keep_only_in_points=True):
+def combine_profiles(out_points, in_points, scale_factor, scale_bank_distance=False):
 
-    dist_ttr_in = float([point for point in out_points if point['properties']['code'] in ('22', '22R')][-1]['properties']['afstand'])
-    dist_ttr_uit = float([point for point in out_points if point['properties']['code'] in ('22', '22R')][-1]['properties']['afstand'])
+    dist_ttr_in = float([point for point in in_points if point['properties']['code'] in ('22', '22R', '22r')][-1]['properties']['afstand'])
+    dist_ttr_uit = float([point for point in out_points if point['properties']['code'] in ('22', '22R', '22r')][-1]['properties']['afstand'])
 
     # todo:sort
     # todo: check and warning, don't throw exception
 
     # step1: distance
+    count_22 = 0
     for point in out_points:
 
-        if point['properties']['code'] in ['99', '22', '22R']:
+        if point['properties']['code'] in ['22', '22R', '22r', '22L', '22l']:
             point['properties']['afstand'] = float(point['properties']['afstand']) * scale_factor
-        elif point['properties']['code'] == '1':
+            count_22 += 1
+        elif count_22 == 0:
             if scale_bank_distance:
                 point['properties']['afstand'] = float(point['properties']['afstand']) * scale_factor
-
-        elif point['properties']['code'] == '2':
+        elif count_22 == 2:
             if scale_bank_distance:
                 point['properties']['afstand'] = float(point['properties']['afstand']) * scale_factor
             else:
                 point['properties']['afstand'] = float(point['properties']['afstand']) - dist_ttr_uit + dist_ttr_in
+        else:
+            point['properties']['afstand'] = float(point['properties']['afstand']) - dist_ttr_uit + dist_ttr_in
 
     # step 2: match inpeiling
     for key in ['_bk_nap', '_ok_nap']:
@@ -57,23 +62,25 @@ def combine_profiles(out_points, in_points, scale_factor, scale_bank_distance=Fa
         array_level = np.array([point['properties'][key]
                                for point in out_points if point['properties'].get(key) is not None])
 
-
-        left = out_points[0]['properties']['afstand']
-        right = out_points[-1]['properties']['afstand']
+        left = round(out_points[0]['properties']['afstand'], 8)
+        right = round(out_points[-1]['properties']['afstand'], 8)
 
         for point in in_points:
-            if left <= point['properties']['afstand'] <= right:
+            if left <= round(point['properties']['afstand'], 8) <= right:
                 point['properties']['uit'+ key] = round(np.interp(point['properties']['afstand'], array_dist,
                                                                   array_level), 2)
 
     return in_points
 
 
-def combine_peilingen(inpeil_file, uitpeil_file, link_table, scale_threshold=0.05, scale_bank_distance=False):
-    in_point_col, in_line_col, in_tt_col, in_errors = import_xml_to_memcollection(inpeil_file, 'z2z1', "Tweede plaats")
+def combine_peilingen(inpeil_file, uitpeil_file, order_inpeiling, order_uitpeiling, loc_inpeiling, loc_uitpeiling,
+                      link_table, scale_threshold=0.05, scale_bank_distance=False):
+    # TODO: Eerste plaats en tweede plaats moeten ook optioneel zijn
+    in_point_col, in_line_col, in_tt_col, in_errors = import_xml_to_memcollection(inpeil_file, order_inpeiling,
+                                                                                  loc_inpeiling)
 
-    uit_point_col, uit_line_col, uit_tt_col, uit_errors = import_xml_to_memcollection(uitpeil_file, 'z2z1',
-                                                                                      "Tweede plaats")
+    uit_point_col, uit_line_col, uit_tt_col, uit_errors = import_xml_to_memcollection(uitpeil_file, order_uitpeiling,
+                                                                                      loc_uitpeiling)
 
     link_dict = link_table_to_dict(link_table)
     out_points = []
@@ -115,8 +122,7 @@ def combine_peilingen(inpeil_file, uitpeil_file, link_table, scale_threshold=0.0
             uit_points,
             in_points,
             scale_factor=prof_width/uit_width,
-            scale_bank_distance=scale_bank_distance,
-            keep_only_in_points=True
+            scale_bank_distance=scale_bank_distance
         )
         out_points += combined_points
 
@@ -125,3 +131,144 @@ def combine_peilingen(inpeil_file, uitpeil_file, link_table, scale_threshold=0.0
     output_collection.writerecords(out_points)
 
     return output_collection
+
+
+def convert_to_metfile(point_col, project, metfile_name, order="z2z1", level_peiling="Inpeiling",
+                       shore_peiling="Inpeiling"):
+
+    with open(metfile_name, 'wb') as csvfile:
+
+        fieldnames = ['regel']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter='|',
+                                quoting=csv.QUOTE_NONE,
+                                quotechar='', escapechar='\\')
+
+        current_profile = ''
+        profile_end = '</PROFIEL>'
+
+        unsorted_points = [p for p in point_col]
+        sorted_points = sorted(unsorted_points, key=lambda x: (x['properties']['prof_ids'],
+                                                               x['properties']['afstand']))
+
+        # Temporarily for users to get used to new method
+        if "," not in project:
+            project = project + "," + project
+
+        # write version
+        version = "<VERSIE>1.0</VERSIE>"
+        writer.writerow({'regel': version})
+
+        # write project data in REEKS
+        proj_nameList = project.split(",")
+        project_tekst = "<REEKS>{0},{1}</REEKS>".format(
+            proj_nameList[0],
+            proj_nameList[1]
+        )
+        writer.writerow({'regel': project_tekst})
+
+        count_22 = 0
+
+        for i, row in enumerate(sorted_points):
+            profile = str(sorted_points[i]['properties']['prof_ids'])
+
+            profile_1 = "{0}_{1}".format(
+                proj_nameList[0],
+                profile
+            )
+
+            profile_2 = "Profiel_{0}".format(profile)
+
+            datum = (str(sorted_points[i]['properties']['datum']))
+
+            try:
+                date = datetime.datetime.strptime(datum, "%d/%m/%Y")
+            except ValueError:
+                try:
+                    date = datetime.datetime.strptime(datum, "%Y%m%d")
+                except ValueError:
+                    date = parser.parse(datum)
+
+            new_date = date.strftime("%Y%m%d")
+            code = sorted_points[i]['properties']['code']
+            tekencode = sorted_points[i]['properties']['tekencode']
+
+            if code in ['22L', '22l', '22R', '22r', '22']:
+                code = 22
+                count_22 += 1
+
+            if count_22 > 2:
+                log.error('meerdere 22 punten aanwezig bij profiel ', profile)
+
+            x_coord = str(sorted_points[i]['properties']['x_coord'])
+            y_coord = str(sorted_points[i]['properties']['y_coord'])
+
+            if code == 22:
+                if level_peiling == "Inpeiling":
+                    upper_level = sorted_points[i]['properties'].get('_bk_nap')
+                    lower_level = sorted_points[i]['properties'].get('_ok_nap')
+                else:
+                    upper_level = sorted_points[i]['properties'].get('uit_bk_nap')
+                    lower_level = sorted_points[i]['properties'].get('uit_ok_nap')
+            elif count_22 != 1:
+                if shore_peiling == "Inpeiling":
+                    upper_level = sorted_points[i]['properties'].get('_bk_nap')
+                    lower_level = sorted_points[i]['properties'].get('_ok_nap')
+                else:
+                    upper_level = sorted_points[i]['properties'].get('uit_bk_nap')
+                    lower_level = sorted_points[i]['properties'].get('uit_ok_nap')
+                    if upper_level is None or lower_level is None:
+                        continue
+            else:
+                upper_level = sorted_points[i]['properties'].get('uit_bk_nap')
+                lower_level = min(sorted_points[i]['properties'].get('uit_ok_nap'),
+                                  sorted_points[i]['properties'].get('_ok_nap'))
+
+                if upper_level < lower_level:
+                    lower_level = upper_level
+
+            # check nieuw profiel
+            if current_profile != profile:
+
+                # check niet eerste profiel -> dan vorige profiel nog afsluiten
+                if current_profile != '':
+                    writer.writerow({'regel': profile_end})
+                    count_22 = 0
+
+                # wegschrijven profielregel
+                profiel_tekst = "<PROFIEL>{0},{1},{2},0.00,NAP,ABS,2,XY,{3},{4},".format(
+                    profile_1,
+                    profile_2,
+                    new_date,
+                    x_coord,
+                    y_coord
+                )
+
+                # writer.writerow({'regel': profiel_tekst})
+                writer.writerow({'regel': profiel_tekst})
+                current_profile = profile
+
+            # wegschrijven meting regels
+            if order == "z2z1":
+                meting_tekst = "<METING>{0},{1},{2},{3},{4},{5},</METING>".format(
+                    code,
+                    tekencode,
+                    x_coord,
+                    y_coord,
+                    upper_level,
+                    lower_level
+                )
+            else:
+                meting_tekst = "<METING>{0},{1},{2},{3},{4},{5},</METING>".format(
+                    code,
+                    tekencode,
+                    x_coord,
+                    y_coord,
+                    lower_level,
+                    upper_level
+                )
+
+            writer.writerow({'regel': meting_tekst})
+
+        writer.writerow({'regel': profile_end})
+
+    return
